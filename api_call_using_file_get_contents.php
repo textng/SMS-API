97eff1f13fd097618293a70949448c24ae3017c8
textng <66287225+textng@users.noreply.github.com>
2020-06-02 03:31:27 +0100

    api_call_using_file_get_contents.php

--- api_call_using_file_get_contents.php
+++ api_call_using_file_get_contents.php
@@ -1,10 +1,19 @@
 //Using file_get_contents
   < ? php
+     
+  //get the parameters here
+  $key="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; //YOUR-ACCOUNT-KEY
+  $sender="Textng"; //YOUR-SENDER-NAME
+  $phone="080XXXXXXXXXX; //PHONE-NUMBER
+  $message=urlencode("Text Message"); //TEXT-MESSAGE
+  $route="6"; //MESSAGE-ROUTE
+   
+  //set your array context options.
   $arrContextOptions=array(
         "ssl"=>array(
             "verify_peer"=>false,
             "verify_peer_name"=>false,
         ),
     );
-  file_get_contents("https://textng.xyz/api/sendsms/?key=YOUR-ACCOUNT-KEY&sender=YOUR-SENDER-NAME&phone=PHONE-NUMBER&message=TEXT-MESSAGE", false, stream_context_create($arrContextOptions));
+  $response=file_get_contents("https://textng.xyz/api/sendsms/?key=$key&sender=$sender&phone=$phone&message=$message&route=$route", false, stream_context_create($arrContextOptions));
   ? >
